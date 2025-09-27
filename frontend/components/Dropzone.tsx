a11e6a3d08ba75d3093a77e64e14137f670ae1a8
Keane Moraes <lordvader3002@gmail.com>
2025-09-27 18:00:00 -0400

    updated the dropzone with a calback function

--- frontend/components/Dropzone.tsx
+++ frontend/components/Dropzone.tsx
@@ -1,23 +1,39 @@
 import { Upload } from "lucide-react";
 
-export function Dropzone() {
+interface DropzoneProps {
+  onFileUpload: (file: File) => void;
+}
+
+export function Dropzone({ onFileUpload }: DropzoneProps) {
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      onFileUpload(file);
+    }
+  };
   return (
     <div className='flex w-full items-center justify-center'>
       <label
         htmlFor='dropzone-file'
-        className='flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600 dark:hover:bg-gray-800'
+        className='flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed bg-accent hover:bg-accent/40'
       >
         <div className='flex flex-col items-center justify-center pt-5 pb-6'>
           <Upload size={32}/>
-          <p className='mb-2 text-sm text-gray-500 dark:text-gray-400'>
+          <p className='mb-2 text-sm text-accent-foreground'>
             <span className='font-semibold'>Click to upload</span> or drag and
             drop
           </p>
-          <p className='text-xs text-gray-500 dark:text-gray-400'>
-            SVG, PNG, JPG or GIF (MAX. 800x400px)
+          <p className='text-xs text-accent-foreground'>
+            PNG or JPG
           </p>
         </div>
-        <input id='dropzone-file' type='file' className='hidden' />
+        <input 
+          id='dropzone-file' 
+          type='file' 
+          className='hidden' 
+          onChange={handleFileChange}
+          accept="image/png,image/jpeg,image/gif"
+        />
       </label>
     </div>
   );
